577522da47b6c4a706d1a7a35f1830e048cebc8c
vaspap1790 <vaspap1790@gmail.com>
2021-04-04 14:01:51 +0300

    message and loader components

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col } from 'react-bootstrap';
 import Packet from '../components/Packet';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
 import { listPackets } from '../actions/packetActions';
 
 const HomeScreen = () => {
@@ -18,9 +20,9 @@ const HomeScreen = () => {
     <>
       <h1>Latest Packets</h1>
       {loading ? (
-        <h2>Loading...</h2>
+        <Loader />
       ) : error ? (
-        <h3>{error}</h3>
+        <Message variant='danger'>{error}</Message>
       ) : (
         <Row>
           {packets.map((packet) => (
