a3895abddf3e516d011e9a7ed0941e02a2154f27
vaspap1790 <vaspap1790@gmail.com>
2021-04-05 09:44:35 +0300

    logout

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -13,7 +13,7 @@ const authUser = asyncHandler(async (req, res) => {
   if (user && (await user.matchPassword(password))) {
     res.json({
       _id: user._id,
-      name: user.username,
+      username: user.username,
       email: user.email,
       isAdmin: user.isAdmin,
       token: generateToken(user._id)
