fa9b00c9ba30a505e4de6c821e221fd72762e5d1
vaspap1790 <vaspap1790@gmail.com>
2021-04-12 23:20:23 +0300

    react-file-icons

--- client/src/screens/PacketFormScreen.js
+++ client/src/screens/PacketFormScreen.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { StyledDropZone } from 'react-drop-zone';
+import { FileIcon, defaultStyles } from 'react-file-icon';
+import Moment from 'react-moment';
 import 'react-drop-zone/dist/styles.css';
 import {
   Form,
@@ -9,10 +11,12 @@ import {
   Col,
   Alert,
   Container,
-  InputGroup
+  InputGroup,
+  Table,
+  OverlayTrigger,
+  Popover
 } from 'react-bootstrap';
 import Loader from '../components/Loader';
-import FormContainer from '../components/FormContainer';
 import {
   validateName,
   validateDescription,
@@ -60,11 +64,28 @@ const PacketFormScreen = ({ history, match }) => {
     e.preventDefault();
   };
 
-  const handleErrorOnClose = () => {};
+  const handleErrorOnClose = () => {
+    //TODO:
+  };
+
+  const removeFromUploadsHandler = () => {
+    //TODO:
+  };
+
+  const popover = (
+    <Popover id='popover-basic'>
+      <Popover.Title as='h3'>Popover right</Popover.Title>
+      <Popover.Content>
+        And here's some <strong>amazing</strong> content. It's very engaging.
+        right?
+      </Popover.Content>
+    </Popover>
+  );
 
   const onDropHandler = async (file, text) => {
     try {
-      const type = file.name.substr(file.name.lastIndexOf('.') + 1);
+      file.timestamp = new Date();
+      console.log(file);
       await setFiles((files) => [...files, file]);
     } catch (error) {
       console.log(error);
@@ -189,17 +210,99 @@ const PacketFormScreen = ({ history, match }) => {
                 <InputGroup>
                   <InputGroup.Prepend>
                     <InputGroup.Text>
-                      <i className='fab fa-ethereum fa-lg'></i>
+                      <i
+                        className='fab fa-ethereum fa-lg'
+                        style={{ color: 'black' }}
+                      ></i>
                     </InputGroup.Text>
                   </InputGroup.Prepend>
-                  <Form.Control id='price' type='number' />
+                  <Form.Control id='price' type='number' min='0' step='0.001' />
                 </InputGroup>
               </Col>
+
               <Col md={6}>
-                <StyledDropZone className='mb-3' onDrop={onDropHandler} />
-                {files.map((file) => (
-                  <Col key={file.name}>{file.name}</Col>
-                ))}
+                <StyledDropZone onDrop={onDropHandler} />
+                <div className='my-3 text-muted'>
+                  <span className='small'>
+                    We'll never share your email with anyone else.{' '}
+                  </span>
+                  <OverlayTrigger
+                    trigger='click'
+                    placement='right'
+                    overlay={popover}
+                  >
+                    <i
+                      class='fas fa-search-plus fa-lg link-icon'
+                      style={{ color: 'black' }}
+                    ></i>
+                  </OverlayTrigger>
+                </div>
+                <Table id='uploadsTable' bordered responsive size='sm'>
+                  <thead>
+                    <tr className='table-dark'>
+                      <th className='uploadsTableHeaders text-center p-2'>
+                        Type
+                      </th>
+                      <th className='uploadsTableHeaders text-center p-2'>
+                        File Name
+                      </th>
+                      <th className='uploadsTableHeaders text-center p-2'>
+                        Date
+                      </th>
+                      <th className='uploadsTableHeaders text-center p-2'>
+                        Action
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {files.map((file) => (
+                      <tr key='file.name'>
+                        <td
+                          style={{ width: '1rem', verticalAlign: 'middle' }}
+                          className='p-2 small'
+                        >
+                          <FileIcon
+                            extension={file.name.substr(
+                              file.name.lastIndexOf('.') + 1
+                            )}
+                            {...defaultStyles[
+                              file.name.substr(file.name.lastIndexOf('.') + 1)
+                            ]}
+                          />
+                        </td>
+                        <td
+                          style={{ verticalAlign: 'middle' }}
+                          className='text-center small'
+                        >
+                          {file.name}
+                        </td>
+                        <td
+                          className='text-muted text-center small'
+                          style={{ verticalAlign: 'middle' }}
+                        >
+                          <Moment format='D MMM YYYY hh:mm:ss'>
+                            {file.timestamp}
+                          </Moment>
+                        </td>
+                        <td
+                          className='text-center small'
+                          style={{ verticalAlign: 'middle' }}
+                        >
+                          {' '}
+                          <Button
+                            title='Remove from Uploads'
+                            className='btn-Icon-Remove btn-sm'
+                            type='button'
+                            variant='light'
+                            onClick={() => removeFromUploadsHandler(file.name)}
+                          >
+                            <i className='fas fa-trash trash'></i>
+                          </Button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </Table>
               </Col>
             </Row>
           </Form>
