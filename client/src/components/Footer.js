0d292d2c7b1b8d8660f498de1cef2b63c8b70b8c
vaspap1790 <vaspap1790@gmail.com>
2021-05-02 00:20:15 +0300

    progress on footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,33 +5,47 @@ const Footer = () => {
   return (
     <footer className='bg-dark'>
       <Container fluid className='px-5 py-4'>
-        <Row>
+        <Row className='mt-2'>
           <Col xs={4} className='d-flex flex-column align-items-center'>
-            <div className='p-2'>Flex item 1</div>
-            <div className='p-2'>Flex item 2</div>
-            <div className='p-2'>Flex item 3</div>
+            <div className='p-1 link-icon blue-hover text-uppercase small'>
+              About Us
+            </div>
+            <div className='p-1 link-icon blue-hover text-uppercase small'>
+              Home
+            </div>
+            <div className='p-1 link-icon blue-hover text-uppercase small'>
+              Why Data Dapp?
+            </div>
           </Col>
           <Col
             xs={4}
             className='d-flex justify-content-center align-items-center'
           >
-            <div className='p-2'>Flex item 1</div>
-            <div className='p-2'>Flex item 2</div>
-            <div className='p-2'>Flex item 3</div>
+            <i class='fab fa-github p-2 link-icon blue-hover fa-2x'></i>
+            <i class='fab fa-linkedin p-2 link-icon blue-hover fa-2x'></i>
+            <i class='fab fa-twitter p-2 link-icon blue-hover fa-2x'></i>
           </Col>
           <Col xs={4} className='d-flex flex-column align-items-center'>
-            <div className='p-2'>Flex item 1</div>
-            <div className='p-2'>Flex item 2</div>
-            <div className='p-2'>Flex item 3</div>
+            <div className='p-1 link-icon blue-hover text-uppercase small'>
+              Terms and Conditions
+            </div>
+            <div className='p-1 link-icon blue-hover text-uppercase small'>
+              Privacy
+            </div>
+            <div className='p-1 link-icon blue-hover text-uppercase small'>
+              What is next?
+            </div>
           </Col>
         </Row>
         <Row>
           <Col xs={4} className='d-flex'></Col>
           <Col xs={4} className='d-flex flex-column align-items-center'>
-            <div className='p-2'>Flex item 1</div>
-            <div className='p-2'>Flex item 2</div>
-            <div className='p-2'>Flex item 3</div>
-            <div className='p-2'>&copy; Data Dapp</div>
+            <div className='p-1 link-icon blue-hover my-4 text-uppercase small'>
+              Contact Us
+            </div>
+            <div className='p-1 text-uppercase small'>
+              &copy; 2021 Data Dapp | All Rights Reserved
+            </div>
           </Col>
           <Col xs={4} className='d-flex'></Col>
         </Row>
